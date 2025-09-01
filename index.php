66748dbcb76d71d3903681a41b66e1424b95bd17
Miguel Cordeiro <135665700+miguelac4@users.noreply.github.com>
2025-09-01 09:59:23 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+switch($_SERVER['HTTP_HOST']) {
+    case 'localhost:8080':
+        // LOCAL
+        header("Location: /page/page_login.php");
+        break;
+    case 'drawline.pt':
+        // SERVER Develop
+        header("Location: page/page_login.php");
+        break;
+    case 'rh360.pt':
+            // SERVER Prod
+        header("Location: page/page_login.php");
+        break;
+}
